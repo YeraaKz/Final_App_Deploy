02e90996cb761ffbaed0513b5d2567b6032c2f74
Yernar <29525@iitu.edu.kz>
2024-05-23 18:13:38 +0500

    MarkdownParser Util class for parsing description

---
+++ src/Util/MarkdownParser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Util;
+
+use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Exception\CommonMarkException;
+
+class MarkdownParser
+{
+    private $converter;
+
+    public function __construct()
+    {
+        $this->converter = new CommonMarkConverter([
+            'html_input' => 'escape',
+            'allow_unsafe_links' => false,
+        ]);
+    }
+
+    /**
+     * @throws CommonMarkException
+     */
+    public function parse(string $markdown): string
+    {
+        return $this->converter->convert($markdown);
+    }
+}
